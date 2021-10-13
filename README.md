29179ab4cd7455478275f2ad1904fb735d93facd
Jamie <74956250+JamieWindfox@users.noreply.github.com>
2021-10-13 20:25:37 +0200

    Undid error with cloning from intelliJ directly

--- README.md
+++
@@ -1,5 +0,0 @@
-# Minesweeper
-Project for FH PROG1 - Minesweeper with JavaFx
-
-Find the Kanban board on 
-https://github.com/JamieWindfox/Minesweeper
--- src/main/java/com/example/minesweeper/MinesweeperApplication.java
+++ src/main/java/com/example/minesweeper/MinesweeperApplication.java
@@ -2,14 +2,7 @@ package com.example.minesweeper;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
-import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
@@ -19,39 +12,14 @@ import java.io.IOException;
 public class MinesweeperApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-
-        createGame(stage);
+        StackPane layout = new StackPane();
+        Scene scene = new Scene(layout, 400, 400, Color.DARKOLIVEGREEN);
+        stage.setTitle("Minesweeper");
+        stage.setScene(scene);
+        stage.show();
     }
 
     public static void main(String[] args) {
         launch();
-    }
-
-    private static void createGame(Stage stage) {
-        GridPane layout = new GridPane();
-        layout.setAlignment(Pos.TOP_CENTER);
-        layout.setHgap(10);
-        layout.setVgap(10);
-        layout.setPadding(new Insets(10, 10, 10, 10));
-
-        GridPane topPane = new GridPane();
-        Label topLabel = new Label("Top Pane");
-        topPane.add(topLabel, 0, 0);
-        layout.add(topPane, 0, 0);
-
-        GridPane gamePane = new GridPane();
-        Label gameLabel = new Label("Game Pane");
-        gamePane.add(gameLabel, 0, 0);
-        layout.add(gamePane, 0, 1);
-
-        GridPane bottomPane = new GridPane();
-        Label bottomLabel = new Label("Bottom Pane");
-        bottomPane.add(bottomLabel, 0, 0);
-        layout.add(bottomPane, 0, 2);
-
-        Scene scene = new Scene(layout, 400, 400);
-        stage.setTitle("Minesweeper");
-        stage.setScene(scene);
-        stage.show();
     }
 }
